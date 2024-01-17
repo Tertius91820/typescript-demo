4c60382d72482ac93856b971ef8019d5745a4985
tertius Jubber <tjubber@gmail.com>
2024-01-17 20:57:09 +0200

    compiled js

--- example.js
+++ example.js
@@ -12,10 +12,27 @@ function isStringLengthGreaterThan(str, len) {
     return str.length > len;
 }
 //Arrya of numbers
-var numbers = [1., 2, 3, 4, 5];
+var numbers = [1, 2, 3, 4, 5];
+// old method  ===>   const num1: Array<number> = [1,2,3,4,5]
 //Object with properties of various types
 var person = {
     name: "john doe",
     age: 30,
-    email: "john.doe@gmsil.com"
+    email: "john.doe@gmail.com"
 };
+//Tupple with two values of different types, enforces type and specific order
+var tuple = ['hello', 123];
+//tuple = [123,"hello"]  ie wrong order
+//Enums with three values (limited options)
+var Colors;
+(function (Colors) {
+    Colors[Colors["Red"] = 0] = "Red";
+    Colors[Colors["Green"] = 1] = "Green";
+    Colors[Colors["Blue"] = 2] = "Blue";
+})(Colors || (Colors = {}));
+//Union type that can either a string or a number
+var unionType = "hello";
+unionType = 123;
+var customType = 12;
+//example usage
+console.log(2, 3);
